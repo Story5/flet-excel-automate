bb7fc35845730dfbff92139671d1c31bcf2b3d08
Story5 <337995545@qq.com>
2023-11-20 10:25:43 +0800

    flet page.add方法

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import flet as ft
 
 def main(page: ft.Page):
     t = ft.Text(value="Hello,world!", color="green")
-    page.controls.append(t)
-    page.update()
-
+    # page.controls.append(t)
+    # page.update()
+    page.add(t) # 相当于 page.controls.append(t) 和 page.update()
 
 ft.app(target=main)
