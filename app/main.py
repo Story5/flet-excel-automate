62526fb814e927e37f62b3eb39afef39c9c93d10
Story5 <337995545@qq.com>
2023-11-20 10:28:44 +0800

    flet Row

--- app/main.py
+++ app/main.py
@@ -6,10 +6,19 @@ def main(page: ft.Page):
     t = ft.Text(value="Hello,world!", color="green")
     # page.controls.append(t)
     # page.update()
-    page.add(t) # 相当于 page.controls.append(t) 和 page.update()
+    page.add(t)  # 相当于 page.controls.append(t) 和 page.update()
+    page.add(ft.Row(
+        controls=[
+            ft.Text("A"),
+            ft.Text("B"),
+            ft.Text("C"),
+        ]
+    ))
     for i in range(10):
         t.value = f"Step {i}"
         page.update()
         time.sleep(1)
+    
+
 
 ft.app(target=main)
