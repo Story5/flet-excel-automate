8ef1f4a1d03037984a3e305dbecbdb06206ffc48
Story5 <337995545@qq.com>
2023-11-20 16:10:58 +0800

    client_storage类shared_preferences

--- app/main.py
+++ app/main.py
@@ -2,14 +2,30 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    def on_dialog_result(e: ft.FilePickerResultEvent):
-        print("Selected files:", e.files)
-        print("Selected file or directory:", e.path)
-    file_picker = ft.FilePicker(on_result=on_dialog_result)
-    page.overlay.append(file_picker)
-    page.update()
-    page.add(ft.ElevatedButton("Choose files...",
-                               on_click=lambda _: file_picker.pick_files(allow_multiple=True)))
+    # strings
+    page.client_storage.set("key", "value")
+
+    # numbers, booleans
+    page.client_storage.set("number.setting", 12345)
+    page.client_storage.set("bool_setting", True)
+
+    # lists
+    page.client_storage.set("favorite_colors", ["read", "green", "blue"])
+
+    # The value is automatically converted back to the original type
+    value = page.client_storage.get("key")
+
+    colors = page.client_storage.get("favorite_colors")
+    # colors = ["read", "green", "blue"]
+
+    # Check if a key exists:
+    page.client_storage.contains_key("key")  # True if the key exists
+    # Get all keys:
+    page.client_storage.get_keys("key-prefix.")
+    # Remove a value:
+    page.client_storage.remove("key")
+    # Clear the storage:
+    page.client_storage.clear()
 
 
 ft.app(target=main)
