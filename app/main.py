e251f03640afce7150fc2dac9a9bf28645548d3d
Story5 <337995545@qq.com>
2023-11-20 14:44:07 +0800

    flet 导航和路由

--- app/main.py
+++ app/main.py
@@ -1,48 +1,39 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.title = "Drag and Drop example"
+    page.title = "Routes Example"
 
-    def drag_accept(e):
-        # get draggable (source) control by its ID
-        src = page.get_control(e.src_id)
-        # update text inside draggable control
-        src.content.content.value = "0"
-        # update text inside drag target control
-        e.control.content.content.value = "1"
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
+                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
+                ],
+            )
+        )
+        if page.route == "/store":
+            page.views.append(
+                ft.View(
+                    "/store",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
         page.update()
 
-    page.add(
-        ft.Row(
-            [
-                ft.Draggable(
-                    group="number",
-                    content=ft.Container(
-                        width=50,
-                        height=50,
-                        bgcolor=ft.colors.CYAN_200,
-                        border_radius=5,
-                        content=ft.Text("1", size=20),
-                        alignment=ft.alignment.center,
-                    ),
-                ),
-                ft.Container(width=100),
-                ft.DragTarget(
-                    group="number",
-                    content=ft.Container(
-                        width=50,
-                        height=50,
-                        bgcolor=ft.colors.PINK_200,
-                        border_radius=5,
-                        content=ft.Text("0", size=20),
-                        alignment=ft.alignment.center,
-                    ),
-                    on_accept=drag_accept,
-                ),
-            ]
-        )
-    )
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 
-ft.app(target=main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
