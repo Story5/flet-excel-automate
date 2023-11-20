dcde73fbca5fd6a1e61af49c4d59a06ed4248f7d
Story5 <337995545@qq.com>
2023-11-20 12:27:39 +0800

    flet GridView

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
+import os
 import flet as ft
+
+os.environ["FLET_WS_MAX_MESSAGE_SIZE"] = "8000000"
 
 
 def main(page: ft.Page):
-    lv = ft.ListView(expand=True, spacing=10)
+    gv = ft.GridView(expand=True, max_extent=150, child_aspect_ratio=1)
+    page.add(gv)
+
     for i in range(5000):
-        lv.controls.append(ft.Text(f"Line {i}"))
-    page.add(lv)
+        gv.controls.append(
+            ft.Container(
+                ft.Text(f"Item {i}"),
+                alignment=ft.alignment.center,
+                bgcolor=ft.colors.AMBER_100,
+                border=ft.border.all(1, ft.colors.AMBER_400),
+                border_radius=ft.border_radius.all(5),
+            )
+        )
+    page.update()
 
 
 ft.app(target=main)
