47587e180018f80b6329dd03a5eb0a36c3185766
Story5 <337995545@qq.com>
2023-11-20 10:39:51 +0800

    flet TextField 使用

--- app/main.py
+++ app/main.py
@@ -20,15 +20,20 @@ def main(page: ft.Page):
             ft.ElevatedButton(text="Say my name!")
         ])
     )
+
     def button_clicked(e):
         page.add(ft.Text('Clicked!'))
-    page.add(ft.ElevatedButton(text='Click me',on_click=button_clicked))
-    for i in range(10):
-        page.controls.append(ft.Text(f"Line {i}"))
-        if i > 4:
-            page.controls.pop(0)
-        page.update()
-        time.sleep(0.3)
+    page.add(ft.ElevatedButton(text='Click me', on_click=button_clicked))
+
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        new_task.focus()
+        new_task.update()
+
+    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
+    page.add(
+        ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
 
 
 ft.app(target=main)
