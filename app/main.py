2b557ad6ae326daa063c2aef817a329e31465a63
Story5 <337995545@qq.com>
2023-11-20 10:23:17 +0800

    flet controls(aka widgets)

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    pass
+    t = ft.Text(value="Hello,world!", color="green")
+    page.controls.append(t)
+    page.update()
 
 
 ft.app(target=main)
