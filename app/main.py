6ad2f3f476305f519c4e9d88ae662235a1918ee6
Story5 <337995545@qq.com>
2023-11-20 12:04:27 +0800

    flet ListView

--- app/main.py
+++ app/main.py
@@ -2,17 +2,10 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    def on_keyboard(e: ft.KeyboardEvent):
-        page.add(
-            ft.Text(
-                f"Key: {e.key}, Shift: {e.shift}, Control: {e.ctrl}, Alt: {e.alt}, Meta: {e.meta}"
-            )
-        )
-
-    page.on_keyboard_event = on_keyboard
-    page.add(
-        ft.Text("Press any key with a combination of CTRL, ALT, SHIFT and META keys...")
-    )
+    lv = ft.ListView(expand=True, spacing=10)
+    for i in range(5000):
+        lv.controls.append(ft.Text(f"Line {i}"))
+    page.add(lv)
 
 
 ft.app(target=main)
