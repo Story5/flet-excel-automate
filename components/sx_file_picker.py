e8eaed954fb7805fd5c1bb26d041e142afffcf21
Story5 <337995545@qq.com>
2023-11-22 15:55:24 +0800

    SXFilePicker优化

--- components/sx_file_picker.py
+++ components/sx_file_picker.py
@@ -1,4 +1,5 @@
 import flet as ft
+import openpyxl
 
 
 class SXFilePicker(ft.UserControl):
@@ -19,7 +20,7 @@ class SXFilePicker(ft.UserControl):
     def build(self):
         self.excel_name = ft.Text()
         self.excel_path = ft.Text(color=ft.colors.GREY)
-        return ft.Column([
+        self.view = ft.Column([
             ft.Row([
                 ft.ElevatedButton(
                     text=self.buttonText,
@@ -30,6 +31,7 @@ class SXFilePicker(ft.UserControl):
             ]),
             self.excel_path,
         ])
+        return self.view
 
     def on_click_choose_excel(self, e):
         self.filePicker.pick_files(
@@ -41,7 +43,27 @@ class SXFilePicker(ft.UserControl):
     def pick_files_result(self, e: ft.FilePickerResultEvent):
         if len(e.files) > 0:
             file_picker_file = e.files[0]
+            path = file_picker_file.path
+
             self.excel_name.value = file_picker_file.name
-            self.excel_path.value = f'文件路径:{file_picker_file.path}'
-            self.update()
-            self.on_result_path(file_picker_file.path)
+            self.excel_path.value = f'文件路径:{path}'
+            self.view.update()
+
+            self.on_result_path(path)
+            self.load_excel(path)
+
+    def load_excel(self, path: str):
+        self.wb = openpyxl.load_workbook(path, data_only=True)
+
+        self.sheets_tab = ft.Tabs(
+            tabs=[ft.Tab(sheet) for sheet in self.wb.sheetnames],
+            scrollable=True,
+            on_change=self.on_tabs_change
+        )
+        self.view.controls.append(self.sheets_tab)
+        self.view.update()
+
+    def on_tabs_change(self, e):
+        sheet_str = self.sheets_tab.tabs[self.sheets_tab.selected_index].text
+        sheetObj = self.wb[sheet_str]
+        print('on_tabs_change:', sheetObj.title)
--- sx_app.py
+++ sx_app.py
@@ -7,7 +7,11 @@ class SXApp(ft.UserControl):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
-        self.stock_excel_path = ''
+
+        self.stock_excel_path = ''  # 库存表excel文件路径
+        self.wb = None
+
+    def build(self):
         self.tonInput = ft.TextField(
             label='请输入需要配货的吨数',
             keyboard_type=ft.KeyboardType.NUMBER
@@ -23,70 +27,47 @@ class SXApp(ft.UserControl):
             label='换60%的货',
             value=False,
         )
-        self.excel_path = None
-        self.wb = None
+
+        self.excel_view = ft.Column(
+            visible=False,
+            controls=[
+                SXFilePicker(
+                    page=self.page,
+                    buttonText='请选择入库excel',
+                    on_result_path=lambda path: self.on_excel_path(path, 1)
+                ),
+            ]
+        )
+        return ft.Column([
+            SXFilePicker(
+                page=self.page,
+                buttonText='请选择库存excel',
+                on_result_path=lambda path: self.on_excel_path(path, 0)
+            ),
+            ft.Divider(),
+            ft.Column([
+                ft.Row([
+                    ft.Text('配货吨数'),
+                    self.tonInput,
+                ]),
+                ft.Row([
+                    ft.Text('换货方式'),
+                    self.typeRadio,
+                    self.sixCheckbox,
+                ])
+            ]),
+            self.excel_view,
+        ])
 
     def on_excel_path(self, path: str, type):
         print('on_excel_path:', path)
         if type == 0:  # 库存表
-            print('库存表:',type)
+            print('库存表:', type)
+            self.stock_excel_path = path
+            self.load_excel(self.stock_excel_path)
         elif type == 1:  # 入库表
-            print('入库表:',type)
-
-    def build(self):
-        self.stock_excel_picker = SXFilePicker(
-            page=self.page,
-            buttonText='请选择库存excel',
-            on_result_path=lambda path: self.on_excel_path(path, 0)
-        )
-
-        self.user_input = ft.Column([
-            ft.Row([
-                ft.Text('配货吨数'),
-                self.tonInput,
-            ]),
-            ft.Row([
-                ft.Text('换货方式'),
-                self.typeRadio,
-                self.sixCheckbox,
-            ])
-        ]
-        )
-        self.excel_view = ft.Column(
-            visible=False,
-            controls=[
-                self.user_input,
-            ]
-        )
-
-        return ft.Column([
-            self.stock_excel_picker,
-            self.excel_view,
-        ])
-
-    def pick_files_result(self, e: ft.FilePickerResultEvent):
-        if len(e.files) > 0:
-            file_picker_file = e.files[0]
-            print(file_picker_file.path)
-            self.load_excel(file_picker_file.path)
+            print('入库表:', type)
 
     def load_excel(self, path):
-        self.stock_excel_path = path
-        self.wb = openpyxl.load_workbook(path, data_only=True)
-
-        self.excel_view.visible = True
-        self.view.update()
-
-        self.sheets_tab = ft.Tabs(
-            tabs=[ft.Tab(sheet)
-                  for sheet in self.wb.sheetnames],
-            scrollable=True,
-            on_change=self.on_tabs_change
-        )
-        self.excel_view.controls.append(self.sheets_tab)
-        self.excel_view.update()
-
-    def on_tabs_change(self, e):
-        sheet_str = self.sheets_tab.tabs[self.sheets_tab.selected_index].text
-        sheetObj = self.wb[sheet_str]
-        print('on_tabs_change:', sheetObj.title)
+        print(path)
+    
